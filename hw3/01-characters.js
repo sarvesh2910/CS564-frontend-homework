b771e1196e39bd61e57511faf2c4b8741b230413
Sarvesh Biradar <sbiradar@pdx.edu>
2023-11-01 15:25:13 -0700

    Added Question 1 solutions.

--- hw3/01-characters.js
+++ hw3/01-characters.js
@@ -1,4 +1,42 @@
 // url for the Thrones API
 const url = 'https://thronesapi.com/api/v2/Characters';
+const list = document.getElementById('list');
 
-fetch(url);
+const createCharacterCard = (character) => {
+  const card = document.createElement('div');
+  card.classList.add('card', 'p-1', 'text-center');
+
+  const image = document.createElement('img');
+  image.src = character.imageUrl;
+  image.alt = `${character.image}`;
+  image.classList.add('characterImage');
+  card.appendChild(image);
+
+  const characterInfo = document.createElement('div');
+  characterInfo.classList.add('characterInfo');
+  card.appendChild(characterInfo);
+
+  const name = document.createElement('h5');
+  name.textContent = character.fullName;
+  name.classList.add('fw-bold');
+  characterInfo.appendChild(name);
+
+  const title = document.createElement('p');
+  title.textContent = character.title;
+  title.classList.add('fw-bold');
+  characterInfo.appendChild(title);
+
+  list.appendChild(card);
+};
+
+const fetchData = async () => {
+  try {
+    const response = await fetch(url);
+    const characters = await response.json();
+    characters.forEach((character) => createCharacterCard(character));
+  } catch (error) {
+    console.error('An error occurred:', error);
+  }
+};
+
+fetchData();
